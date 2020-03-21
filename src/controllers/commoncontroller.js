26ad3091bef981fc94d4682af5071d1c77a880c3
mvoevodskiy <mv@aaa0.ru>
2020-03-21 23:52:17 +0300

    Added sendAttach() method

--- src/controllers/commoncontroller.js
+++ src/controllers/commoncontroller.js
@@ -6,6 +6,30 @@ class MVLUsersController extends MVLoaderBase {
 
     reject = () => false;
 
+    sendAttach = (ctx, params) => {
+        if (typeof params !== 'object') {
+            return;
+        }
+        let parcel = new ctx.BC.config.classes.Parcel();
+        for (let type in params) {
+            if (params.hasOwnProperty(type)) {
+                parcel.attachments[type] = parcel.attachments[type] || [];
+                let attachments = this.MT.makeArray(params[type]);
+                for (let attachment of attachments) {
+                    if (this.MT.isString(attachment)) {
+                        attachment = {
+                            file: attachment,
+                        }
+                    }
+                    attachment.file = process.cwd() + '/' + attachment.file;
+                    parcel.attachments[type].push(attachment);
+                }
+            }
+        }
+        // console.log('COMMON SEMIS. SEND ATTACHMENTS: ', parcel.attachments);
+        return ctx.reply(parcel);
+    }
+
 }
 
 module.exports = MVLUsersController;
